949b501d1e17cdd6d0a0e7b44d2fa849462a67b8
chessco <chesssco@gmail.com>
2024-10-04 19:23:30 -0700

    4 commit

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
-from flask import Flask, jsonify
 import os
 
-app = Flask(__name__)
+from flask import Flask, jsonify, request
+from pinecone import Pinecone
+from pinecone_plugins.assistant.models.chat import Message
 
+app = Flask(__name__) 
 
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+pc = Pinecone(api_key=os.environ.get('PINECONE_API_KEY'))
 
+assistant = pc.assistant.Assistant(assistant_name=os.environ.get('ASSISTANT_NAME'))
 
-if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+def chat_with_assistant(question):
+    chat_context = [Message(content=question)]
+    response = assistant.chat_completions(messages=chat_context)
+    return response.choices[0].message.content
+
+@app.route('/ask', methods=['POST'])
+def ask():
+    data = request.json
+    if not data or 'question' not in data:
+        return jsonify({'error': 'No question provided'}), 400
+
+    question = data['question']
+    answer = chat_with_assistant(question)
+    return jsonify({"answer": answer})
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
